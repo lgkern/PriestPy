cbf4f8239c55407a772409693affe134df66d989
Lucas Kern <lucasgilbertokern@gmail.com>
2016-09-25 20:47:59 -0300

    fixes

--- src/priestLogger.py
+++ src/priestLogger.py
@@ -4,12 +4,12 @@ from logging.handlers import TimedRotatingFileHandler
 class PriestLogger:
 
 	def __init__(self):
-		logHandler = TimedRotatingFileHandler("C:\\lucas\\PriestPy\\logs\\HowToPriest",when="midnight")
+		logHandler = TimedRotatingFileHandler("C:\\lucas\\PriestPy\\logs\\HowToPriest",when="midnight",backupCount=365)
 		logFormatter = logging.Formatter('%(asctime)s - %(message)s')
 		logHandler.setFormatter( logFormatter )
-		logger = logging.getLogger( 'H2PLogger' )
-		logger.addHandler( logHandler )
-		logger.setLevel( logging.INFO )
+		self.logger = logging.getLogger( 'H2PLogger' )
+		self.logger.addHandler( logHandler )
+		self.logger.setLevel( logging.INFO )
 	
 	def log(self, message):
-		logger.info(message.channel.name + ' - ' + message.author.name+': ' + message.content)
+		self.logger.info(message.channel.name + ' - ' + message.author.name+': ' + message.content)
