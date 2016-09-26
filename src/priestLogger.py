5d389edf404533be24887acb4baa9ca2c61987e3
Lucas Kern <lucasgilbertokern@gmail.com>
2016-09-26 12:54:16 -0300

    New log location

--- src/priestLogger.py
+++ src/priestLogger.py
@@ -4,7 +4,7 @@ from logging.handlers import TimedRotatingFileHandler
 class PriestLogger:
 
 	def __init__(self):
-		logHandler = TimedRotatingFileHandler("C:\\lucas\\PriestPy\\logs\\HowToPriest",when="midnight",backupCount=365)
+		logHandler = TimedRotatingFileHandler("C:\\lucas\\PriestPy\\Dropbox\\logs\\HowToPriest",when="midnight",backupCount=365)
 		logFormatter = logging.Formatter('%(asctime)s - %(message)s')
 		logHandler.setFormatter( logFormatter )
 		self.logger = logging.getLogger( 'H2PLogger' )
