9066f699ad63abafc33e932db9bff5d4ebfcce10
Lucas Kern <lucasgilbertokern@gmail.com>
2016-10-10 15:41:58 -0300

    Logger fix

--- src/priestLogger.py
+++ src/priestLogger.py
@@ -7,9 +7,9 @@ class PriestLogger:
 	def __init__(self):
 		self.logHandler = TimedRotatingFileHandler('C:\\lucas\\PriestPy\\Dropbox\\logs\\HowToPriest',when='midnight',backupCount=365)
 		self.logFormatter = logging.Formatter('%(asctime)s - %(message)s')
-		self.logHandler.setFormatter( logFormatter )
+		self.logHandler.setFormatter( self.logFormatter )
 		self.logger = logging.getLogger( 'H2PLogger' )
-		self.logger.addHandler( logHandler )
+		self.logger.addHandler( self.logHandler )
 		self.logger.setLevel( logging.INFO )
 		self.logHandler.createLock()
 	
