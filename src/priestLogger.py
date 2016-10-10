6c37f0ddcfa9276ba481d01a50b041e272c1146f
Lucas Kern <lucasgilbertokern@gmail.com>
2016-10-10 15:41:00 -0300

    Logger fix

--- src/priestLogger.py
+++ src/priestLogger.py
@@ -5,20 +5,20 @@ from time import sleep
 class PriestLogger:
 
 	def __init__(self):
-		logHandler = TimedRotatingFileHandler('C:\\lucas\\PriestPy\\Dropbox\\logs\\HowToPriest',when='midnight',backupCount=365)
-		logFormatter = logging.Formatter('%(asctime)s - %(message)s')
-		logHandler.setFormatter( logFormatter )
+		self.logHandler = TimedRotatingFileHandler('C:\\lucas\\PriestPy\\Dropbox\\logs\\HowToPriest',when='midnight',backupCount=365)
+		self.logFormatter = logging.Formatter('%(asctime)s - %(message)s')
+		self.logHandler.setFormatter( logFormatter )
 		self.logger = logging.getLogger( 'H2PLogger' )
 		self.logger.addHandler( logHandler )
 		self.logger.setLevel( logging.INFO )
-		logHandler.createLock()
+		self.logHandler.createLock()
 	
 	def log(self, message):
-		logHandler.acquire()
+		self.logHandler.acquire()
 		try:
 			self.logger.info(message.channel.name + ' - ' + message.author.name+': ' + message.content)
-			logHandler.release()
+			self.logHandler.release()
 		except:
-			logHandler.release()
+			self.logHandler.release()
 		
 			
