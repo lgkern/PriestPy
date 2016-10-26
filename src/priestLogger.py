e1724689b9d41cc1dbccc7b8a48e299df00c59a5
Lucas Kern <lucasgilbertokern@gmail.com>
2016-10-26 12:37:08 -0200

    Changes to the logger

--- src/priestLogger.py
+++ src/priestLogger.py
@@ -1,24 +1,24 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from time import sleep
+import string
 
 class PriestLogger:
 
-	def __init__(self):
-		self.logHandler = TimedRotatingFileHandler('C:\\lucas\\PriestPy\\Dropbox\\logs\\HowToPriest',when='midnight',backupCount=365)
-		self.logFormatter = logging.Formatter('%(asctime)s - %(message)s')
-		self.logHandler.setFormatter( self.logFormatter )
-		self.logger = logging.getLogger( 'H2PLogger' )
-		self.logger.addHandler( self.logHandler )
-		self.logger.setLevel( logging.INFO )
-		self.logHandler.createLock()
-	
-	def log(self, message):
-		self.logHandler.acquire()
-		try:
-			self.logger.info(message.channel.name + ' - ' + message.author.name+': ' + message.content)
-			self.logHandler.release()
-		except:
-			self.logHandler.release()
-		
-			
+    def __init__(self):
+        self.logHandler = TimedRotatingFileHandler('C:\\lucas\\PriestPy\\Dropbox\\logs\\HowToPriest',when='midnight',backupCount=365)
+        self.logFormatter = logging.Formatter('%(asctime)s - %(message)s')
+        self.logHandler.setFormatter( self.logFormatter )
+        self.logger = logging.getLogger( 'H2PLogger' )
+        self.logger.addHandler( self.logHandler )
+        self.logger.setLevel( logging.INFO )
+        self.logHandler.createLock()
+        self.printable = set(string.printable)
+    
+    def log(self, message):
+        self.logHandler.acquire()
+        channelName = message.channel.name if not message.channel.is_private else 'PM'
+        self.logger.info(channelName + ' - ' + message.author.name+': ' + ''.join(filter(lambda x: x in self.printable, message.content)))
+        self.logHandler.release()
+      
+            
